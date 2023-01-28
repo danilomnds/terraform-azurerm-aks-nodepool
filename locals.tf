bd4a521ec2012f62903c6c7034d4b68db6d80cd4
Danilo Mendes <damendes@timbrasil.com.br>
2023-01-28 13:53:05 -0300

    editing default tags

--- locals.tf
+++ locals.tf
@@ -3,8 +3,5 @@ locals {
     deployedby  = "Terraform"
     provider    = "azr"
     create_date = formatdate("DD/MM/YY hh:mm", timeadd(timestamp(), "-3h"))
-    type        = "paas"
-    resource    = "kubernetes"
-    layer       = "application"
   }
 }
